a2668bee60179e2e9bca5422574eac6a0c087616
prakashprajapat2008 <prakash.prajapat.cg@gmail.com>
2026-09-15 16:44:13 +0530

    file added

---
+++ 03-Operators/1-Arithmetc operators/Operators.js
@@ -0,0 +1,105 @@
+//1. Addition +
+let collection1=15000;
+let collection2=12500;
+let totalCollection=collection1+collection2
+console.log(`The total collection is ${totalCollection}`)
+
+let readMorning=18;
+let readEvening=25;
+let totalPage=readEvening+readMorning;
+console.log(`the total page read is ${totalPage}`)
+
+let soldMonday=125;
+let soldTuesday=178;
+let totalItemSold=soldMonday+soldTuesday;
+console.log(`the total item sold is ${totalItemSold}`)
+
+
+//2. Subtraction -
+let totalSeat=80;
+let occupiedSeat=53;
+let emptySeat=totalSeat-occupiedSeat;
+console.log(`The total empty seat are availabe is ${emptySeat}`)
+
+let totalMarks=500;
+let marksLose=35;
+let finalMark=totalMarks-marksLose
+console.log(`The final marks is ${finalMark}`)
+
+let totalBox=2500;
+let sendBox=875;
+let remainingBox=totalBox-sendBox;
+console.log(`The remaining box in warehouse are ${remainingBox}`)
+
+
+
+//3. Multiplication *
+let cost=45;
+let notebookQuantity=8;
+let totalPrice=cost*notebookQuantity;
+console.log(`the total price of 8 notebook is ${totalPrice}`)
+
+let perHour=120;
+let hour=6;
+let totalBottels=perHour*hour;
+console.log(`The total bottel after 6 hour are ${totalBottels}`)
+
+let totalRow=7;
+let plant=15;
+let totalPlant=totalRow*plant;
+console.log(`the total plant in 7 row is ${totalPlant}`)
+
+
+
+//4. Division /
+let pencils=144;
+let totalStudent=12;
+let eachStudent=pencils/totalStudent;
+console.log(`The number of pencile each student get ${eachStudent}`)
+
+let travelDistance=360;
+let timeTaken=6;
+let average=travelDistance/timeTaken;
+console.log(`The average speed of the train is ${average}`)
+
+let totalMoney=72000;
+let depertments=9;
+let eachDepertment=totalMoney/depertments;
+console.log(`the moiney each depertment get is ${eachDepertment}`)
+
+
+//5. Modulus %
+let totalStudents=53;
+let groupOf=5;
+let leftStudent=totalStudents%groupOf
+console.log(`the remenaning student are ${leftStudent}`)
+
+let totalCandies=128;
+let packOf=10;
+let leftCandies=totalCandies%packOf;
+console.log(`the remaning candies from the pack ${leftCandies}`)
+
+let num = Number(prompt("Enter a number:"));
+if (num % 2 === 0) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
+
+//6. Exponentiation **
+let side=6;
+let volume=side**3;
+console.log(`the volume of cube is ${volume} cm`)
+
+let bacteria=1;
+let hours=4;
+let bacteriaAfter=bacteria*(2**hours);
+console.log(`the bacteria after the 4 hours is ${bacteriaAfter}`)
+
+let oneCell=9;
+let sides=4;
+let totalCells=oneCell*(sides**2);
+console.log(`The total number of cells are ${totalCells}`)
+
+
+
